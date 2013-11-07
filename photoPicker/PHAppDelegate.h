5bccc7e3928382c337a068efcfd1e24811f5dade
Lagspoon <lagspoon@gmail.com>
2013-11-07 17:04:24 +0100

    Initial Commit

---
+++ photoPicker/PHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PHAppDelegate.h
+//  photoPicker
+//
+//  Created by Olivier Delecueillerie on 07/11/2013.
+//  Copyright (c) 2013 Olivier Delecueillerie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
