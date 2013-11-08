f2559a37843ec5657cc60f5a29da885c5f07fc90
Lagspoon <lagspoon@gmail.com>
2013-11-08 14:11:21 +0100

    App run well, no modification left

--- photoPicker/PHAppDelegate.h
+++ photoPicker/PHAppDelegate.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "PHCameraVC.h"
 
 @interface PHAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) PHCameraVC *viewController;
 
 @end
